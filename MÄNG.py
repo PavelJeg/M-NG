6321b9a0d6c90302d91c15d3f5a11dbcb3bb4c9c
opilane <opilane@HP-CZC2349HTN>
2023-01-30 14:49:23 +0200

    Add project files.

---
+++ MÄNG.py
@@ -0,0 +1,29 @@
+﻿import random 
+
+user_action = input("Tee oma valik – kivi, käärid või paber: ")
+possible_actions = ["kivi", "paber", "käärid"]
+computer_action = random.choice(possible_actions)
+print(f"\nSina valisid {user_action}, arvuti valis {computer_action}.\n")
+if user_action == computer_action:
+    print(f"Mõlemad kasutajad on valinud {user_action}. Ничья!!")
+elif user_action == "kivi":
+    if computer_action == "käärid":
+        print("Kivi lööb käärid! Sina võitsid!")
+    else:
+        print("Paber võidab kivi! Sa kaotasid.")
+elif user_action == "paber":
+    if computer_action == "kivi":
+        print("Paber võidab kivi! Sina võitsid!")
+    else:
+        print("Käärid lõikavad paberit! Sa kaotasid.")
+elif user_action == "käärid":
+    if computer_action == "paber":
+        print("Käärid lõikavad paberit! Sina võitsid!")
+    else:
+        print("Kivi lööb käärid! sa kaotasid.")
+
+play_again = "" 
+play_again = input("Mängime uuesti? (+/-): ") 
+if play_again.lower() != "+":
+  break
+   
