b0268bfc7cde62e4f3521246dfa9dc634129c083
PavelJeg <120379029+PavelJeg@users.noreply.github.com>
2023-03-30 12:28:56 +0300

    Update MÄNG.py

--- MÄNG.py
+++ MÄNG.py
@@ -2,30 +2,30 @@ import random
 import tkinter as tk
 
 window = tk.Tk()
-window.title("Камень, ножницы, бумага")
+window.title("Kivi,paber,käärid")
 
 def play_game(user_choice):
-    possible_actions = ["камень", "бумага", "ножницы"]
+    possible_actions = ["kivi", "paber", "käärid"]
     computer_choice = random.choice(possible_actions)
-    result_text.set(f"Вы выбрали {user_choice}, компьютер выбрал {computer_choice}.\n")
+    result_text.set(f"Sina valisid {user_choice}, arvuti valis {computer_choice}.\n")
 
     if user_choice == computer_choice:
         result_text.set(result_text.get() + "Ничья!")
-    elif user_choice == "камень":
-        if computer_choice == "ножницы":
-            result_text.set(result_text.get() + "Камень бьет ножницы! Вы выиграли!")
+    elif user_choice == "kivi":
+        if computer_choice == "Käärid":
+            result_text.set(result_text.get() + "Kivi lööb käärid! Sa oled võitnud!")
         else:
-            result_text.set(result_text.get() + "Бумага побеждает камень! Вы проиграли.")
-    elif user_choice == "бумага":
-        if computer_choice == "камень":
-            result_text.set(result_text.get() + "Бумага побеждает камень! Вы выиграли!")
+            result_text.set(result_text.get() + "Paber võidab kivi! Sa kaotasid.")
+    elif user_choice == "paber":
+        if computer_choice == "kivi":
+            result_text.set(result_text.get() + "Paber võidab kivi! Sa oled võitnud!")
         else:
-            result_text.set(result_text.get() + "Ножницы режут бумагу! Вы проиграли.")
-    elif user_choice == "ножницы":
-        if computer_choice == "бумага":
-            result_text.set(result_text.get() + "Ножницы режут бумагу! Вы выиграли!")
+            result_text.set(result_text.get() + "Käärid lõikavad paberit! Sa kaotasid.")
+    elif user_choice == "Käärid":
+        if computer_choice == "paber":
+            result_text.set(result_text.get() + "Käärid lõikavad paberit! Sa oled võitnud!")
         else:
-            result_text.set(result_text.get() + "Камень бьет ножницы! Вы проиграли.")
+            result_text.set(result_text.get() + "Kivi lööb käärid! Sa kaotasid.")
 
     play_again_button.grid(row=3, column=1)
 
@@ -36,20 +36,20 @@ def reset_game():
 choices_frame = tk.Frame(window)
 choices_frame.grid(row=1, column=1)
 
-rock_button = tk.Button(choices_frame, text="Камень", command=lambda: play_game("камень"))
+rock_button = tk.Button(choices_frame, text="Kivi", command=lambda: play_game("kivi"))
 rock_button.pack(side=tk.LEFT, padx=5)
 
-paper_button = tk.Button(choices_frame, text="Бумага", command=lambda: play_game("бумага"))
+paper_button = tk.Button(choices_frame, text="Paber", command=lambda: play_game("paber"))
 paper_button.pack(side=tk.LEFT, padx=5)
 
-scissors_button = tk.Button(choices_frame, text="Ножницы", command=lambda: play_game("ножницы"))
+scissors_button = tk.Button(choices_frame, text="Käärid", command=lambda: play_game("Käärid"))
 scissors_button.pack(side=tk.LEFT, padx=5)
 
 result_text = tk.StringVar()
 result_label = tk.Label(window, textvariable=result_text, font=("Helvetica", 18), wraplength=400)
 result_label.grid(row=2, column=1)
 
-play_again_button = tk.Button(window, text="Сыграть ещё раз")
+play_again_button = tk.Button(window, text="mängi uuesti")
 
 
 import random 
@@ -81,4 +81,3 @@ while True:
     play_again = input("Mängime uuesti? (+/-): ") 
     if play_again.lower() != "+":
         break
-
